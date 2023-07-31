71aeb0c6349c8139cbc930c1b36f900dd52acf1a
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2023-07-31 13:24:07 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Houssam Eddine Baba Bendermel on 31/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
